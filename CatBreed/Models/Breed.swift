bea1e436894b61613d26e3c20ab5addbaa149b4c
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-07-20 16:12:32 +0300

    work with project

---
+++ CatBreed/Models/Breed.swift
@@ -0,0 +1,38 @@
+//
+//  Breed.swift
+//  CatBreed
+//
+//  Created by Анна Белова on 20.07.2024.
+//
+
+import Foundation
+
+struct Breed: Decodable {
+    let breed: String
+    let country: String
+    let origin: String
+    let coat: String
+    let pattern: String
+}
+
+struct Link: Decodable {
+    let url: String?
+    let label: String
+    let active: Bool
+}
+
+struct CatBreedInfo: Decodable {
+    let current_page: Int
+    let data: [Breed]
+    let first_page_url: String
+    let from: Int
+    let last_page: Int
+    let last_page_url: String
+    let links: [Link]
+    let next_page_url: String
+    let path: String
+    let per_page: Int
+    let prev_page_url: String?
+    let to: Int
+    let total: Int
+}
---
+++ CatBreed/ViewControllers/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewController.swift
+//  CatBreed
+//
+//  Created by Анна Белова on 20.07.2024.
+//
+
+import UIKit
+
+final class MainViewController: UITableViewController {
+    
+    let url = URL(string: "https://catfact.ninja/breeds")!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchBreed()
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
+
+// MARK: - Networking
+extension MainViewController {
+    private func fetchBreed(){
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let breedInfo = try decoder.decode(CatBreedInfo.self, from: data)
+                print(breedInfo)
+            } catch let error {
+             print(error)
+            }
+       
+        }.resume()
+    }
+}
